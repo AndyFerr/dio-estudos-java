74a6319b30a54692e3be016ad287f55757e71f3e
AndyFerr <anfs@ic.ufal.br>
2024-06-13 22:24:29 -0300

    teste conta desafio

--- Fundamentos-Java/src/App.java
+++ Fundamentos-Java/src/App.java
@@ -1,9 +1,42 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int a = 1;
+import java.util.Scanner;
 
-        String b = a==1 ? "SIm" : "Não";
-        System.out.println(b);
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    double attSaldo = scanner.nextDouble();
+                    saldo = attSaldo;
+                    System.out.printf("Saldo atual: %.1d", saldo);
+                    break;
+                case 2:
+                    double valorSacar = scanner.nextDouble();
+                    if (valorSacar < saldo){
+                      saldo = (saldo - valorSacar);
+                      System.out.printf("Saldo atual: %.1d", saldo);
+                    }else{
+                      System.out.print("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    System.out.printf("Saldo atual: %.1d", saldo);
+                    break;
+                case 0:
+                    System.out.print("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.print("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
     }
 }
