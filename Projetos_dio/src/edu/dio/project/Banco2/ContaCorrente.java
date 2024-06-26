7c4d03f69a91cd96eede09f75b2002fd7ea80bfe
AndyFerr <anfs@ic.ufal.br>
2024-06-26 15:28:53 -0300

    Projeto banco2
    Signed-off-by: AndyFerr <anfs@ic.ufal.br>

---
+++ Projetos_dio/src/edu/dio/project/Banco2/ContaCorrente.java
@@ -0,0 +1,16 @@
+package edu.project.Banco2;
+
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+
+}
